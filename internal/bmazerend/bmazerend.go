87a9fdb642112f4fa76fb486421a8c5cb1f335d0
Lounarisnia <r.arswendo.r@gmail.com>
2026-05-09 15:51:45 +0700

    wip: naive line function

--- internal/bmazerend/bmazerend.go
+++ internal/bmazerend/bmazerend.go
@@ -1,6 +1,7 @@
 package bmazerend
 
 import (
+	"math"
 	"unsafe"
 
 	"github.com/lunarisnia/boomer-maze/internal/bmazerend/window"
@@ -11,12 +12,21 @@ const (
 	screenHeight = 600
 )
 
+func line(ax int32, ay int32, bx int32, by int32, fb *window.Framebuffer, color uint32) {
+	for t := 0.0; t < 1.0; t += 0.002 {
+		x := int32(math.Round(float64(ax) + float64(bx-ax)*t))
+		y := int32(math.Round(float64(ay) + float64(by-ay)*t))
+		fb.SetPixel(x, y, color)
+	}
+}
+
 func draw(ctx *window.WindowContext) {
 	// render
-	ctx.Framebuffer.Clear(0xFF000000)                     // black
-	ctx.Framebuffer.SetPixel(100, 100, window.ColorWhite) // red dot
-	ctx.Framebuffer.SetPixel(200, 100, window.ColorWhite) // red dot
-	ctx.Framebuffer.SetPixel(200, 400, window.ColorWhite) // red dot
+	ctx.Framebuffer.Clear(0xFF000000) // black
+
+	line(100, 100, 220, 120, ctx.Framebuffer, window.ColorGreen)
+	line(220, 120, 200, 400, ctx.Framebuffer, window.ColorGreen)
+	line(200, 400, 100, 100, ctx.Framebuffer, window.ColorGreen)
 
 	// blit framebuffer to texture
 	ctx.Texture.Update(nil, unsafe.Pointer(&ctx.Framebuffer.Pixels[0]), int(ctx.Framebuffer.Width)*4)
