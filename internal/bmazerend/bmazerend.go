0d3f578cd7a3eb9a38c09cda48858405c4883a7f
Lounarisnia <r.arswendo.r@gmail.com>
2026-05-09 16:28:47 +0700

    feat: 10k lines

--- internal/bmazerend/bmazerend.go
+++ internal/bmazerend/bmazerend.go
@@ -1,7 +1,10 @@
 package bmazerend
 
 import (
+	"fmt"
 	"math"
+	"math/rand"
+	"time"
 	"unsafe"
 
 	"github.com/lunarisnia/boomer-maze/internal/bmazerend/window"
@@ -37,25 +40,43 @@ func line(ax int32, ay int32, bx int32, by int32, fb *window.Framebuffer, color 
 	}
 }
 
+var lines []func(ctx *window.WindowContext)
+
 func draw(ctx *window.WindowContext) {
 	// render
 	ctx.Framebuffer.Clear(0xFF000000) // black
 
-	line(100, 100, 220, 120, ctx.Framebuffer, window.ColorGreen)
-	line(220, 120, 200, 400, ctx.Framebuffer, window.ColorGreen)
-	line(200, 400, 100, 100, ctx.Framebuffer, window.ColorGreen)
+	// line(100, 100, 220, 120, ctx.Framebuffer, window.ColorGreen)
+	// line(220, 120, 200, 400, ctx.Framebuffer, window.ColorGreen)
+	// line(200, 400, 100, 100, ctx.Framebuffer, window.ColorGreen)
+	start := time.Now()
+	for _, l := range lines {
+		l(ctx)
+	}
 
 	// blit framebuffer to texture
 	ctx.Texture.Update(nil, unsafe.Pointer(&ctx.Framebuffer.Pixels[0]), int(ctx.Framebuffer.Width)*4)
 	ctx.Renderer.Clear()
 	ctx.Renderer.Copy(ctx.Texture, nil, nil)
 	ctx.Renderer.Present()
+	fmt.Println("RenderTime: ", time.Since(start))
 }
 
 func Run() {
 	win := window.New("Test", screenWidth, screenHeight)
 	defer win.Destroy()
 	win.SetDrawFunction(draw)
+
+	for range 10_000 {
+		ax := rand.Int31() % screenWidth
+		ay := rand.Int31() % screenHeight
+		bx := rand.Int31() % screenWidth
+		by := rand.Int31() % screenHeight
+		col := rand.Uint32()
+		lines = append(lines, func(ctx *window.WindowContext) {
+			line(ax, ay, bx, by, ctx.Framebuffer, col)
+		})
+	}
 
 	win.Run()
 }
