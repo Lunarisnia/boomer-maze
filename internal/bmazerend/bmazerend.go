e947427d847b521af8d4a88e05c677bc13dbbf86
Lounarisnia <r.arswendo.r@gmail.com>
2026-05-27 14:16:30 +0700

    feat: rend

--- internal/bmazerend/bmazerend.go
+++ internal/bmazerend/bmazerend.go
@@ -1,10 +1,7 @@
 package bmazerend
 
 import (
-	"fmt"
 	"math"
-	"math/rand"
-	"time"
 	"unsafe"
 
 	"github.com/lunarisnia/boomer-maze/internal/bmazerend/window"
@@ -20,7 +17,6 @@ func swap[T any](a T, b T) (T, T) {
 }
 
 func line(ax int32, ay int32, bx int32, by int32, fb *window.Framebuffer, color uint32) {
-	// TODO: Implement the error based optimization
 	steep := math.Abs(float64(ax-bx)) < math.Abs(float64(ay-by))
 	if steep {
 		ax, ay = swap(ax, ay)
@@ -38,7 +34,6 @@ func line(ax int32, ay int32, bx int32, by int32, fb *window.Framebuffer, color 
 		} else {
 			fb.SetPixel(x, int32(y), color)
 		}
-		// y += float64(by-ay) / float64(bx-ax)
 		iError += 2 * int32(math.Abs(float64(by-ay)))
 		if iError > bx-ax {
 			if by > ay {
@@ -51,43 +46,25 @@ func line(ax int32, ay int32, bx int32, by int32, fb *window.Framebuffer, color 
 	}
 }
 
-var lines []func(ctx *window.WindowContext)
-
 func draw(ctx *window.WindowContext) {
 	// render
 	ctx.Framebuffer.Clear(0xFF000000) // black
 
-	// line(100, 100, 220, 120, ctx.Framebuffer, window.ColorGreen)
-	// line(220, 120, 200, 400, ctx.Framebuffer, window.ColorGreen)
-	// line(200, 400, 100, 100, ctx.Framebuffer, window.ColorGreen)
-	start := time.Now()
-	for _, l := range lines {
-		l(ctx)
-	}
+	line(100, 100, 220, 120, ctx.Framebuffer, window.ColorGreen)
+	line(220, 120, 200, 400, ctx.Framebuffer, window.ColorGreen)
+	line(200, 400, 100, 100, ctx.Framebuffer, window.ColorGreen)
 
 	// blit framebuffer to texture
 	ctx.Texture.Update(nil, unsafe.Pointer(&ctx.Framebuffer.Pixels[0]), int(ctx.Framebuffer.Width)*4)
 	ctx.Renderer.Clear()
 	ctx.Renderer.Copy(ctx.Texture, nil, nil)
 	ctx.Renderer.Present()
-	fmt.Println("RenderTime: ", time.Since(start))
 }
 
 func Run() {
 	win := window.New("Test", screenWidth, screenHeight)
 	defer win.Destroy()
 	win.SetDrawFunction(draw)
-
-	for range 10_000 {
-		ax := rand.Int31() % screenWidth
-		ay := rand.Int31() % screenHeight
-		bx := rand.Int31() % screenWidth
-		by := rand.Int31() % screenHeight
-		col := rand.Uint32()
-		lines = append(lines, func(ctx *window.WindowContext) {
-			line(ax, ay, bx, by, ctx.Framebuffer, col)
-		})
-	}
 
 	win.Run()
 }
